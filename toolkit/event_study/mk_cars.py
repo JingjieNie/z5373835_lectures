8318ea91ea922666a9bc9edfce7bc879436c6b75
JingjieNie <129850085+JingjieNie@users.noreply.github.com>
2023-04-04 20:29:32 +1000

    Add files via upload

---
+++ toolkit/event_study/mk_cars.py
@@ -0,0 +1,6 @@
+'''Step 4: mk_cars.mk_cars_df(ret_df, event_df) --> dataframe
+  Creates a dataframe with the CARs for each event of interest, where:
+  `ret_df` : dataframe
+      Output of `mk_rets.mk_ret_df`
+  `event_df` : dataframe
+      Output of `mk_events.mk_event_df`'''
---
+++ toolkit/event_study/mk_events.py
@@ -0,0 +1,3 @@
+'''Step 3: mk_events.mk_event_df(tic) --> dataframe
+  Process the recommendations in `<rec csv>` and create a data frame with the
+  events of interest.'''
