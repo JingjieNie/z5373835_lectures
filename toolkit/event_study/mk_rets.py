26c0c74e1e47653462f21f4adc401eb0a8b4fd28
JingjieNie <129850085+JingjieNie@users.noreply.github.com>
2023-04-04 21:45:20 +1000

    Add files via upload

---
+++ toolkit/event_study/mk_rets.py
@@ -0,0 +1,3 @@
+'''Step 2: mk_rets.mk_ret_df(tic) --> dataframe
+  For a given `tic`, create stock returns from `<prc csv>` and join the market
+  returns.'''
---
+++ toolkit/toolkit_config.py
@@ -0,0 +1,4 @@
+import os
+
+PRJDIR = r'C:\Users\njj15\PycharmProjects\toolkit'
+DATADIR = os.path.join(PRJDIR, 'data')
